3e046a037f12806f584b905772a39a3687da8c46
Lorenzo Sommaruga <lorenzo.sommaruga@supsi.ch>
2017-02-23 15:21:57 +0100

    Add new file, create ws interface

---
+++ SommaWebService.java
@@ -0,0 +1,9 @@
+// WS Interface, ASO Course Example LS Feb 2017
+import javax.jws.WebService;
+
+@WebService(targetNamespace = "http://www.supsi.ch")
+public interface SommaWebService {
+
+    public int somma(int add1, int add2);
+
+}
